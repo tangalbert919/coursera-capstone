6d86d0e5d434470f9cdcb32b9b004c67ca0692d6
Albert Tang <tangalbert919@yahoo.com>
2023-12-27 16:54:41 -0600

    Drop throttling

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -138,14 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '2/minute',
-        'user': '10/minute'
-    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
